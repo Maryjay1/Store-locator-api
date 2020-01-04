e903ebf86a757f0768c0616aaccf187481129c6a
mary joseph <mary_jay@icloud.com>
2020-01-04 23:32:50 +0000

    tested get request

--- server.js
+++ server.js
@@ -8,4 +8,16 @@ dotenv.config({path: './config/config.env' });
 
 const app = express();
 
+//body parser
+app.use(express.json());
+
+//Enable cors
+
+app.use(cors());
+
+app.get('/api/v1/stores',(req, res) => {
+res.send('Hello')});
+
 const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
